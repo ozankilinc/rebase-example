f5e4d1beea115b6093cd3615de475bca26dfc88d
ozankilinc <ozankilinc42@gmail.com>
2023-06-01 20:37:44 +0300

    Add PostRebase

--- src/main/java/com/rebase/example/rebaseexample/service/RebaseService.java
+++ src/main/java/com/rebase/example/rebaseexample/service/RebaseService.java
@@ -1,9 +1,13 @@
 package com.rebase.example.rebaseexample.service;
 
 import com.rebase.example.rebaseexample.model.request.GetRebaseRequest;
+import com.rebase.example.rebaseexample.model.request.PostRebaseRequest;
 import com.rebase.example.rebaseexample.model.response.GetRebaseResponse;
+import com.rebase.example.rebaseexample.model.response.PostRebaseResponse;
 
 public interface RebaseService {
 
     GetRebaseResponse getRebase(GetRebaseRequest request);
+
+    PostRebaseResponse postRebase(PostRebaseRequest request);
 }
